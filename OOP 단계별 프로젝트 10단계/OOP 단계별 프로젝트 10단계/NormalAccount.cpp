0510f3c53230caab52d39069c58da0e8bb39817c
창민 박 <cmnowhere@naver.com>
2020-09-08 16:49:54 +0900

    par k changmin

--- OOP 단계별 프로젝트 10단계/OOP 단계별 프로젝트 10단계/NormalAccount.cpp
+++ OOP 단계별 프로젝트 10단계/OOP 단계별 프로젝트 10단계/NormalAccount.cpp
@@ -3,18 +3,18 @@
 
 using namespace std;
 
-NormalAccount::NormalAccount(String accid,int money, String name, double r) :Account(accid,money,name), ratio(r) {}
+NormalAccount::NormalAccount(String accid, int money, String name, double r) :Account(accid, money, name), ratio(r) {}
 
 void NormalAccount::GetCheckInfo()
 {
-	cout << "ÀÌ¸§ : " << GetName() << endl;
-	cout << "°èÁÂ¹øÈ£ : " << GetAccID() << endl;
-	cout << "ÀÜ¾× : " << GetMoney() << endl << endl;
+	cout << "   ÀÌ¸§ : " << GetName() << endl << endl;
+	cout << "   °èÁÂ¹øÈ£ : " << GetAccID() << endl << endl;
+	cout << "   ÀÜ¾× : " << GetMoney() << endl;
 }
 
 void NormalAccount::GetDeposit(int m)
-{	
+{
 	int b = GetMoney();
-	b += m+(int)(m * ratio/100);
+	b += m + (int)(m * ratio / 100);
 	Account::GetDeposit(b);
 }
